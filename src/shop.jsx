66a6336c7f40b365a69887fca2d2433ec387afad
Bellamcr <izabella.mcr@gmail.com>
2023-12-03 16:22:42 -0700

    css in progress

--- src/shop.jsx
+++ src/shop.jsx
@@ -1,8 +1,45 @@
 export default function Shop() {
-    return (
-      <>
-        <h1>All products</h1>
-        <p>.</p>
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>All products</h1>
+      <div className="cards">
+        <div className="card">
+          <img
+            className="card_img"
+            src="hinese-evergreen.jpg"
+            alt="Chinese Evergreen"
+          />
+          <h2>Chinese Evergreen</h2>
+          <p className="price">$40</p>
+          <p>
+            <button>Add to Cart</button>
+          </p>
+        </div>
+        <div className="card">
+          <img
+            className="card_img"
+            src="hinese-evergreen.jpg"
+            alt="Chinese Evergreen"
+          />
+          <h2>Chinese Evergreen</h2>
+          <p className="price">$40</p>
+          <p>
+            <button>Add to Cart</button>
+          </p>
+        </div>
+        <div className="card">
+          <img
+            className="card_img"
+            src="hinese-evergreen.jpg"
+            alt="Chinese Evergreen"
+          />
+          <h2>Chinese Evergreen</h2>
+          <p className="price">$40</p>
+          <p>
+            <button>Add to Cart</button>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
