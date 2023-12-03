2356148081c5cf36309a3e9cee3b722675620578
Bellamcr <izabella.mcr@gmail.com>
2023-12-02 18:37:53 -0700

    Adding react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,8 @@ import { StrictMode } from "react";
 // import { useState, useEffect } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
 import Navbar from "./navbar";
 import Home from "./home";
 import Shop from "./shop";
@@ -18,32 +12,36 @@ import About from "./about";
 import Contact from "./contact";
 import Error from "./error";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+function App() {
+  const rootElement = document.getElementById("root");
+  const root = createRoot(rootElement);
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/shop",
-    element: <Shop />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <Error />,
+    },
+    {
+      path: "/shop",
+      element: <Shop />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
+    },
+  ]);
 
-root.render(
-  <StrictMode>
-    <Navbar />
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+  root.render(
+    <StrictMode>
+      <Navbar />
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+}
+
+export default App;
