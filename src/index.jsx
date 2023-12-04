312a49add4d5154c34538cd680a9387794b8bc93
Bellamcr <izabella.mcr@gmail.com>
2023-12-04 00:16:21 -0700

    fonts

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import '/.fonts/aller_bd-webfont.woff'
-import '/.fonts/aller_bd-webfont.woff2'
-import '/.fonts/caviardreams-webfont.woff'
-import '/.fonts/caviardreams-webfont.woff2'
+import './fonts/aller_bd-webfont.woff'
+import './fonts/aller_bd-webfont.woff2'
+import './fonts/caviardreams-webfont.woff'
+import './fonts/caviardreams-webfont.woff2'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
